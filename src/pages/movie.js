2519b823c97ac914bdb34f4808cd2418cd03b1ca
Rob1-alt <robin.pautigny@hetic.net>
2021-03-22 18:18:44 +0100

    fix the genre

--- src/pages/movie.js
+++ src/pages/movie.js
@@ -5,11 +5,14 @@ import { useParams } from "react-router";
 const api_key = "435c8880fa41fdbe5fba133c47f78d2b";
 const BASE_URL = "https://api.themoviedb.org/3";
 const getImage = (path) => `https://image.tmdb.org/t/p/w300/${path}`;
-const getVideo = (path) => `https://www.youtube.com/watch?v=${path}`;
+// const getVideo = (path) => `https://www.youtube.com/watch?v=${path}`;
 
 function MoviePage() {
   const params = useParams()
-  const [movieInfo, setMovieInfo] = useState({ title : "" });
+  const [movieInfo, setMovieInfo] = useState({ title : "", genres : [] });
+  const [dataloaded, setdataLoaded] = useState(false);
+
+
 
 
   useEffect(() =>{
@@ -36,16 +39,14 @@ function MoviePage() {
                 <div className="maindiv">
                   <div className="second div">
                     <img src = { getImage(movieInfo.poster_path) } alt ="Poster"/>
-                     {/* // {movieInfo.release_date}
-                    // {movieInfo.popularity} */}
-                      
-                  </div>
+
+                    </div>
                   <div className="third-div">
-                
-                    <p>publique : {movieInfo.popularity} </p>
                     <div className="divNumber">
                       <p>Date de sortie : {movieInfo.release_date}</p>
                       <p>Durée du film : {movieInfo.runtime} minutes </p>
+                      <p>{movieInfo.status}</p>
+                       {movieInfo.genres[1].name}
                     </div>
                     <div className="divsynopsis">
                         <h3>Synopsis</h3>
