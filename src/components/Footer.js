e6a8af75f064169d2e64dbc1679b012a43e9b5e8
Rob1-alt <robin.pautigny@hetic.net>
2021-03-25 09:36:29 +0100

    adding the wave, fix the react

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Footer.css";
-import redwave from "../pages/img/wave.png";
 import logoW from "../pages/img/logo-white.png";
 import logoF from "../pages/img/logoFacebook.png";
 import logoI from "../pages/img/logoInstagram.png";
@@ -8,9 +7,9 @@ import logoL from "../pages/img/logoLinkedin.png";
 import line from "../pages/img/line.png";
 
 function Footer() {
-  return (
+return (
     <div className="container3">
-        <img src={redwave} className="wave" alt="red wave footer"/> 
+        <svg className ="redWave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#BF223B" fill-opacity="1" d="M0,224L80,208C160,192,320,160,480,154.7C640,149,800,171,960,181.3C1120,192,1280,192,1360,192L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
         <div className="footerLinks">
             <img src={logoW} className="logoW" alt="logo cinepick white"/>
             <ul>
